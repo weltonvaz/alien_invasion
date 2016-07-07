d90022e2775dd7d6b7e38193972843d4560b677b
Welton Vaz <weltonvaz@gmail.com>
2016-07-07 17:23:01 -0300

    Primeira comitada

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,12 +10,17 @@ def run_game():
     screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Alien Invasion")
 
+    # Define a cor de fundo
+    bg_color = (230, 230, 230)
+
     # Inicia o laço principal do jogo
     while True:
         # Observa eventos de teclado e de mouse
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+        # Redesenha a tela a cada passagem pelo laço
+        screen.fill(bg_color)
         # Deixe a tela mais recente visível
         pygame.display.flip()
 
