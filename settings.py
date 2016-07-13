ddd15057692cbde2d9f178e1389fdb9a9843a050
Welton Vaz <weltonvaz@gmail.com>
2016-07-13 08:52:19 -0300

    Arquivo de configuração

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """ Uma classe para armazenar todas as configurações da Invasão Alienígea."""
+
+    def __init__(self):
+        """ Iniciaza as configuraçẽos do jogo."""
+        # configurações da tela
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
