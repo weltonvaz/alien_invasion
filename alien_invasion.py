85eec19610aa2c282a028c19e6c950459d372d94
Welton Vaz <weltonvaz@gmail.com>
2016-07-26 10:23:06 -0300

    mudanças no codigo em 26/07/2016

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,15 +3,15 @@
 
 import sys
 import pygame
+from settings import Settings
+
 
 def run_game():
-    # Inicializa o jogo e cria um objeto para tela
+    # Inicializa o pygame, as configurações e o objeto screen
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-
-    # Define a cor de fundo
-    bg_color = (230, 230, 230)
 
     # Inicia o laço principal do jogo
     while True:
@@ -20,7 +20,7 @@ def run_game():
             if event.type == pygame.QUIT:
                 sys.exit()
         # Redesenha a tela a cada passagem pelo laço
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
         # Deixe a tela mais recente visível
         pygame.display.flip()
 
